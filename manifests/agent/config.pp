aa0d5c0ef8242ea31f474b9d80ff7b65f2e2bd27
Bas Grolleman <bgrolleman@emendo-it.nl>
2014-05-13 13:34:44 +0200

    On empty whitelist_ip we should not use it.

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -3,7 +3,8 @@
 # Configure check_mk agent
 #
 class check_mk::agent::config (
-  $ip_whitelist = $check_mk::agent::ip_whitelist,
+  #  $ip_whitelist = $check_mk::agent::ip_whitelist,
+  $ip_whitelist = ['192.168.20.1'],
   $port         = $check_mk::agent::port,
   $server_dir   = $check_mk::agent::server_dir,
   $use_cache    = $check_mk::agent::use_cache,
@@ -18,21 +19,21 @@ class check_mk::agent::config (
         $server = "${server_dir}/check_mk_agent"
       }
       if $ip_whitelist {
-        $only_from = join($ip_whitelist, ' ')
-      }
-      else {
+        $ip_whitelist_string = join($ip_whitelist, ' ')
+        $only_from = "127.0.0.1 ${ip_whitelist_string}"
+      } else {
         $only_from = undef
       }
       xinetd::service { 'check_mk':
         port      => $port,
         server    => $server,
         user      => $user,
-        only_from => "127.0.0.1 $only_from"
+        only_from => $only_from
       }
       xinetd::service { 'check-mk-agent':
         ensure => 'absent',
-        port      => $port,
-        server    => $server,
+        port   => $port,
+        server => $server,
       }
     }
     default: {
