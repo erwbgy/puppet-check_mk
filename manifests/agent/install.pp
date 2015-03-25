726f8e03c21722370c095af7a0ede35f1c770e65
unknown <apatectawfik@DSVITEC088D.lan.apa.at>
2015-03-25 15:31:34 +0100

    set correct rpm name and remove logwatch requirement

--- manifests/agent/install.pp
+++ manifests/agent/install.pp
@@ -14,40 +14,24 @@ class check_mk::agent::install (
         ensure => directory,
       }
     }
-    file { "${workspace}/check_mk-agent-${version}.noarch.rpm":
+    file { "${workspace}/check-mk-agent-${version}.noarch.rpm":
       ensure  => present,
-      source  => "${filestore}/check_mk-agent-${version}.noarch.rpm",
+      source  => "${filestore}/check-mk-agent-${version}.noarch.rpm",
       require => Package['xinetd'],
     }
-    file { "${workspace}/check_mk-agent-logwatch-${version}.noarch.rpm":
-      ensure  => present,
-      source  => "${filestore}/check_mk-agent-logwatch-${version}.noarch.rpm",
-      require => Package['xinetd'],
-    }
-    package { 'check_mk-agent':
+
+    package { 'check-mk-agent':
       ensure   => present,
       provider => 'rpm',
-      source   => "${workspace}/check_mk-agent-${version}.noarch.rpm",
-      require  => File["${workspace}/check_mk-agent-${version}.noarch.rpm"],
+      source   => "${workspace}/check-mk-agent-${version}.noarch.rpm",
+      require  => File["${workspace}/check-mk-agent-${version}.noarch.rpm"],
     }
-    package { 'check_mk-agent-logwatch':
-      ensure   => present,
-      provider => 'rpm',
-      source   => "${workspace}/check_mk-agent-logwatch-${version}.noarch.rpm",
-      require  => [
-        File["${workspace}/check_mk-agent-logwatch-${version}.noarch.rpm"],
-        Package['check_mk-agent'],
-      ],
-    }
+
   }
   else {
-    package { 'check_mk-agent':
+    package { 'check-mk-agent':
       ensure  => present,
       require => Package['xinetd'],
-    }
-    package { 'check_mk-agent-logwatch':
-      ensure  => present,
-      require => Package['check_mk-agent'],
     }
   }
 }
