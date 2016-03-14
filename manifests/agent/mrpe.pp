dafde6d21dccd5f746331c1ff89de043f5ed7536
Baptiste Grenier <bgrenier@gnubila.fr>
2016-03-14 09:55:35 +0100

    check_mk::agent::mrpe: add missing at EOL

--- manifests/agent/mrpe.pp
+++ manifests/agent/mrpe.pp
@@ -25,18 +25,18 @@ define check_mk::agent::mrpe (
   $mrpe_config_file = $::operatingsystem ? {
     centos  => '/etc/check-mk-agent/mrpe.cfg',
     redhat  => '/etc/check-mk-agent/mrpe.cfg',
-    default => undef
+    default => undef,
   }
 
   if ( $mrpe_config_file ) {
     if ! defined(Concat[$mrpe_config_file]) {
       concat { $mrpe_config_file:
-        ensure => 'present'
+        ensure => 'present',
       }
     }
     concat::fragment { "${name}-mrpe-check":
       target  => $mrpe_config_file,
-      content => "${name} ${command}\n"
+      content => "${name} ${command}\n",
     }
   } else {
     fail("Creating mrpe.cfg is unsupported for operatingsystem ${::operatingsystem}")
