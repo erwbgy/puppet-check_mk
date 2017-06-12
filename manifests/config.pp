237bb66cc326f0c858aace98a80dc7f9cbde194f
Frederic Wou <fwou@gnubila.fr>
2017-06-12 15:57:45 +0200

    "concat::fragment" : "ensure" no longer supported, use "source" instead

--- manifests/config.pp
+++ manifests/config.pp
@@ -48,7 +48,7 @@ class check_mk::config (
   }
   # local list of hosts is in /omd/sites/${site}/etc/check_mk/all_hosts_static and is appended
   concat::fragment { 'all-hosts-static':
-    ensure => "${etc_dir}/check_mk/all_hosts_static",
+    source => "${etc_dir}/check_mk/all_hosts_static",
     target => "${etc_dir}/check_mk/main.mk",
     order  => 18,
   }
@@ -76,11 +76,11 @@ class check_mk::config (
     }
   }
   # local config is in /omd/sites/${site}/etc/check_mk/main.mk.local and is appended
-  concat::fragment { 'all-hosts-static':
-    source => "${etc_dir}/check_mk/all_hosts_static",
+  concat::fragment { 'check_mk-local-config':
+    source => "${etc_dir}/check_mk/main.mk.local",
     target => "${etc_dir}/check_mk/main.mk",
-    order  => 18,
-  }
+    order  => 99,
+}
   # re-read config if it changes
   exec { 'check_mk-refresh':
     command     => "/bin/su -l -c '${bin_dir}/check_mk -I' ${site}",
